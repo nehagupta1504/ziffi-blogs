c55fe4e7430a143864c81e9b0761e6f4d2752979
Neha Gupta <nehagupta150498@gmail.com>
2024-03-08 00:48:35 +0530

    added more header icons

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,10 +6,27 @@ const Header = () => {
   return (
     <header className={styles["header"]}>
       <div className={styles["logo"]}>
-        <Link className={styles["logo-img"]} href="/">ZiffyB.</Link>
+        <Link className={styles["logo-img"]} href='/'>
+          ZiffyB.
+        </Link>
       </div>
       <div className={styles["header-options"]}>
-        <Link className={styles['add-blog-link']} href="/blog/addblog">Add Blog</Link>
+        <Link className={styles["add-blog-link"]} href='/blog/addblog'>
+          <div className={styles["header-option"]}>
+            <span className='icon icon-write'></span>
+            <span className={styles["header-item"]}>Write</span>
+          </div>
+        </Link>
+        <Link className={styles["notification"]} href='/notication'>
+          <div className={styles["header-option"]}>
+            <span className='icon icon-bell'></span>
+          </div>
+        </Link>
+        <div className={styles["user-icon"]}>
+            <div className={styles["header-option"]}>
+              <span className='icon icon-user'></span>
+            </div>
+        </div>
       </div>
     </header>
   );
