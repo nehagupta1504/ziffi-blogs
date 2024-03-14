35e1bbebc772ab109b6d5dd50d00fca4b5f988cf
Neha Gupta <nehagupta150498@gmail.com>
2024-03-15 00:32:52 +0530

    added title funtionality also

--- src/pages/blog/addblog.tsx
+++ src/pages/blog/addblog.tsx
@@ -59,31 +59,9 @@ export default function BlogDetail(props: any) {
         <title>Ziffi-Blogs</title>
       </Head>
       <main className={styles["add-blog"]}>
-        <div className={styles.container}>
-          <h1>Create a New Blog Post</h1>
+        <div className={styles["add-blog__title"]}>
+         <input className={styles["add-blog__title__input"]} placeholder="Enter your title here..." onChange={(e)=>{handleChange("title", e.target.value)}}></input>
         </div>
-        {/* <div>
-          <form className={styles["blog-form"]}>
-            <label htmlFor='title' className={styles["form-label"]}>
-              Title:
-            </label>
-            <input type='text' id='title' name='title' value={title} className={styles["form-input"]} required onChange={handleChange} />
-
-            <label htmlFor='author' className={styles["form-label"]}>
-              Author:
-            </label>
-            <input type='text' id='author' name='author' value={author} className={styles["form-input"]} required onChange={handleChange} />
-
-            <label htmlFor='content' className={styles["form-label"]}>
-              Content:
-            </label>
-            <textarea id='content' name='content' value={content} className={styles["form-textarea"]} rows={8} required onChange={handleChange}></textarea>
-
-            <button type='submit' value='Submit' className={styles["submit-button"]} onClick={addPost} disabled={buttonDisabled}>
-              {loading ? <i className='fa fa-spinner fa-spin'></i> : "Submit"}
-            </button>
-          </form>
-        </div> */}
         <div className={styles["add-blog-container"]}>
           <div className={styles["editor"]}>
             <PostEditor handleChange={handleChange}/>
